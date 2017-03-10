a721d7099ac0cd30dfa9badafddf426cd4647eea
Robert Seymour <seymourr@aston.ac.uk>
2017-03-10 16:13:25 +0000

    Further Updates to Code

--- mi_tort_visual_data_concat.m
+++ mi_tort_visual_data_concat.m
@@ -1,6 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Script to compute PAC comodulograms for the pre and post grating period,
 % using V1 VE data
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -27,26 +29,11 @@ for sub = 1:length(subject)
 
     %% Get comod for post grating (0.3 to 1.5s) period
     [MI_matrix] = calc_MI(VE_V1,[0.3 1.5],[6 20],[30 80],'no');
-    matrix_post = MI_matrix; save matrix_post matrix_post; clear MI_matrix
-    
-    % Plot for sanity
-%     figure('color', 'w'); subplot(2,1,1);
-%     pcolor(6:1:20,30:2:80,matrix_post)
-%     shading interp; colormap(jet)
-%     ylabel('Frequency (Hz)'); xlabel('Phase (Hz)')
-%     title(sprintf('Comod post-grating Subject %s',subject{sub}))
-%     pbaspect([1.5,1,1])    
+    matrix_post = MI_matrix; save matrix_post matrix_post; clear MI_matrix  
 
     % Get comod for pre grating (-1.5 to -0.3s) period
     [MI_matrix] = calc_MI(VE_V1,[-1.5 -0.3],[6 20],[30 80],'no')
     matrix_pre = MI_matrix; save matrix_pre matrix_pre;
-    
-%     % Plot for sanity
-%     subplot(2,1,2); pcolor(6:1:20,30:2:80,matrix_pre)
-%     shading interp; colormap(jet);
-%     ylabel('Amplitude (Hz)'); xlabel('Phase (Hz)');
-%     title(sprintf('Comod pre-grating Subject %s',subject{sub}))
-%     pbaspect([1.5,1,1])    
     
     % Compare MI matrix for pre-grating to post-grating
     comb = (matrix_post - matrix_pre)./(matrix_post + matrix_pre);
